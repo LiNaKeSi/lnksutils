8a8023b292bdb6c028d05e1c662a320fdf1d44d3
snyh <snyh@snyh.org>
2020-07-11 19:13:16 +0800

    reexec: adjust priority of command choosing

--- reexec/reexec.go
+++ reexec/reexec.go
@@ -22,7 +22,7 @@ func Register(name string, initializer func()) {
 // Init is called as the first part of the exec process and returns true if an
 // initialization function was called.
 func Init(names ...string) bool {
-	for _, name := range append(names, path.Base(os.Args[0])) {
+	for _, name := range append([]string{path.Base(os.Args[0])}, names...) {
 		initializer, exists := registeredInitializers[name]
 		if exists {
 			initializer()
